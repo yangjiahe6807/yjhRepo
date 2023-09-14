37fa48dd37e00f9af692c49ed3cbe48686ea9250
yangjiahe6807 <85986670+yangjiahe6807@users.noreply.github.com>
2023-09-14 14:23:44 +0800

    Add files via upload

---
+++ ExcelTOORACLE/MAIN.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExcelTOORACLE
+{
+    public partial class MAIN : Form
+    {
+        public MAIN()
+        {
+            InitializeComponent();
+        }
+    }
+}
