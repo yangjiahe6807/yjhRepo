c8200f5f24dad1a03b5f936195c069d78fde8e7f
yangjiahe6807 <85986670+yangjiahe6807@users.noreply.github.com>
2023-11-20 12:31:44 +0800

    Add files via upload

---
+++ ToDo/My Project/factory1.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .factory('factory1', factory1);
+
+    factory1.$inject = ['$http'];
+
+    function factory1($http) {
+        var service = {
+            getData: getData
+        };
+
+        return service;
+
+        function getData() { }
+    }
+})();
